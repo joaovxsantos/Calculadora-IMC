3baccdfc1e8dc491d6a4a405d1e656118ed2f9ef
João Vitor <joaovitor55670@gmail.com>
2022-06-23 17:27:18 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -8,22 +8,26 @@ let p = document.getElementsByTagName("p")[0];
 botao.addEventListener("click", msg);
 
 
-function msg(){
-    let altura2 = altura.value*altura.value;
-    let count = Math.trunc(peso.value/altura2);
-   if(count < 18.5){
-    p.innerText = "Olá "  + nome.value + "!" + " seu IMC é de " + count + " e você está abaixo do peso.";
-   }
-   else if(count >= 18.5 && count<= 24.9){
-    p.innerText = "Olá " + nome.value + "!" + " seu IMC é de " + count + " está tudo certo!";
-   }
-   else if(count >=25 && count <=29.9){
-    p.innerText = "Olá " + nome.value + "!" + " seu IMC é de " + count + " e você está com sobrepeso.";
-   }
-   else if(count >=30 && count <=39.9){
-    p.innerText = "Olá " + nome.value +  "!" +" seu IMC é de " + count + " e você está com obesidade.";
-   }
-   else{
-    p.innerText = "Olá " + nome.value + "!" + " seu IMC é de " + count + " e você está com obesidade grave.";
-   }
+function msg() {
+    let altura2 = altura.value * altura.value;
+    let count = Math.trunc(peso.value / altura2);
+
+    if (count < 18.5) {
+        p.innerText = "Olá " + nome.value + "!" + " seu IMC é de " + count + " e você está abaixo do peso.";
+    }
+    else if (count >= 18.5 && count <= 24.9) {
+        p.innerText = "Olá " + nome.value + "!" + " seu IMC é de " + count + " está tudo certo!";
+    }
+    else if (count >= 25 && count <= 29.9) {
+        p.innerText = "Olá " + nome.value + "!" + " seu IMC é de " + count + " e você está com sobrepeso.";
+    }
+    else if (count >= 30 && count <= 39.9) {
+        p.innerText = "Olá " + nome.value + "!" + " seu IMC é de " + count + " e você está com obesidade.";
+    }
+    else if (count >= 40) {
+        p.innerText = "Olá " + nome.value + "!" + " seu IMC é de " + count + " e você está com obesidade grave.";
+    }
+    else {
+        p.innerText = "Erro! Preencha as opçoes e tente novamente"
+    }
 }
